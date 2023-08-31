160c4be2ab29cfb18a9e6e96daf03d988772e932
GuitounYoucef <guitxyoucef@gmail.com>
2023-08-31 21:05:03 +0100

    First commit

--- projects/simple-feature/schematics/ng-add/index.ts
+++ projects/simple-feature/schematics/ng-add/index.ts
@@ -1,11 +1,11 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask} from '@angular-devkit/schematics/tasks'
+//import { NodePackageInstallTask} from '@angular-devkit/schematics/tasks'
 
 export function ngAdd(_options: any): Rule {
     
     return (tree: Tree, _context: SchematicContext) => {
         _context.logger.info('Installing dependencies ...');
-        _context.addTask(new NodePackageInstallTask())
+        
       return tree;
     };
   }
--- projects/simple-feature/schematics/ui-component/index.ts
+++ projects/simple-feature/schematics/ui-component/index.ts
@@ -1,22 +1,22 @@
-import { apply, applyTemplates, chain, externalSchematic, MergeStrategy, mergeWith, move, Rule, url } from "@angular-devkit/schematics";
+import { apply, applyTemplates, chain, mergeWith, move, Rule, url } from "@angular-devkit/schematics";
 import { strings, normalize } from "@angular-devkit/core";
 import { ComponentSchema } from "./ComponentSchema";
 
 export function ComponentGenerator(options: ComponentSchema): Rule {
   return () => {
- /*   const templateSource = apply(
+    const templateSource = apply(
       url('./files'), [
         applyTemplates({
           classify: strings.classify,
           dasherize: strings.dasherize,
           name: options.name
         }),
-        move(normalize(`/${options.path}/${strings.dasherize(options.name)}`))
+        move(normalize(`/${options.path}/${options.name}`))
       ]
-    )*/
+    )
     return chain([
-      externalSchematic('@schematics/angular', 'component', options),
-     // mergeWith(templateSource, MergeStrategy.Overwrite)
+    //  externalSchematic('@schematics/angular', 'component', options),
+      mergeWith(templateSource)
     ])
   }
 }
