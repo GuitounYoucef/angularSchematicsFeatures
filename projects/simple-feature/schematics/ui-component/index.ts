c054023086c1f98d80bc33dc6496f1486d24f6e3
GuitounYoucef <68856205+GuitounYoucef@users.noreply.github.com>
2023-09-13 00:10:51 +0100

    main.ts update

--- projects/simple-feature/schematics/ui-component/index.ts
+++ projects/simple-feature/schematics/ui-component/index.ts
@@ -1,8 +1,8 @@
 import { apply, applyTemplates, chain, mergeWith, move, Rule, url, Tree, SchematicContext, SchematicsException,  } from "@angular-devkit/schematics";
 import { strings, normalize } from "@angular-devkit/core";
 import { ComponentSchema } from "./ComponentSchema";
-import {  addImportToModule,  getSourceNodes,  } from '@schematics/angular/utility/ast-utils';
-import {  applyToUpdateRecorder,  InsertChange , Change} from '@schematics/angular/utility/change';
+import {  getSourceNodes,  } from '@schematics/angular/utility/ast-utils';
+import {  InsertChange , Change,NoopChange} from '@schematics/angular/utility/change';
 import * as ts from 'typescript';
 
 
@@ -15,7 +15,8 @@ caommands:
 3- npm adduser --registry http://localhost:4873/
 4- npm run build
 5- npm run build --prefix projects/simple-feature
-6- dist/simple-feature: npm publish
+6-1 cd dist/simple-feature
+6-2 npm publish
 
 add library to your project
 1- npm add simple-feature
@@ -31,11 +32,16 @@ export function addObjectToArrayChange (context: string, tree: Tree, nodeName:st
 
   let nodes: ts.Node[] = getSourceNodes(node);
 
+  const existNode = nodes.find(n => n.kind === ts.SyntaxKind.Identifier && n.getText() === context);
+  if (existNode) {
+    return new NoopChange();
+  }
+
   // find providers node by checking the SyntaxKind to be Identifier and by checking the node text to be providers
   const providersNode = nodes.find(n => n.kind === ts.SyntaxKind.Identifier && n.getText() === nodeName);
 
   if (!providersNode || !providersNode.parent) {
-    throw new SchematicsException(`providers people variable in ${path}`);
+    throw new SchematicsException(`providers variable in ${path}`);
   }
  return new InsertChange(path, providersNode.getEnd()+3,'\n' + context)
 }
@@ -59,57 +65,56 @@ function unppercase(str: string): string {
   return str.toUpperCase();
 }
 
+function addToNode(tree: Tree,path:string,newNode: string,parentNode:string):void
+{
+
+  let change = addObjectToArrayChange(newNode, tree,parentNode,path);
+  const declarationRecorder = tree.beginUpdate(path);
+  if (change instanceof InsertChange) {
+      declarationRecorder.insertLeft(change.pos,change.toAdd);
+  }
+  tree.commitUpdate(declarationRecorder);
+}
+/******************************************************************************** */
+
+function addImportToFile(tree: Tree,path:string,importNode: string):void
+{
+  const declarationRecorder = tree.beginUpdate(path);
+  let importChange=new InsertChange(path, 0,importNode+'\n' )
+  if (importChange instanceof InsertChange) {
+    declarationRecorder.insertLeft(importChange.pos,importChange.toAdd);
+}  
+  tree.commitUpdate(declarationRecorder);
+}
+
 export function ComponentGenerator(options: ComponentSchema): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.logger.info('Adding library Module to the app...');
-    /* 
-    const modulePath = '/src/app/app.module.ts';
-    if (!tree.exists(modulePath)) {
-      throw new SchematicsException(`The file ${modulePath} doesn't exists...`);
-    }
-    const recorder = tree.beginUpdate(modulePath);
-
-    const text = tree.read(modulePath);
-
-    if (text === null) {
-      throw new SchematicsException(`The file ${modulePath} doesn't exists...`);
-    }
-
-    const source = ts.createSourceFile(
-      modulePath,
-      text.toString(),
-      ts.ScriptTarget.Latest,
-      true
-    );
-
-    applyToUpdateRecorder(recorder,
-      addImportToModule(source, modulePath, 'provideStore', '@ngrx/store')
-    );
-
-    tree.commitUpdate(recorder); */
 
 
-    //************************************************************************************* */
-    let path='/src/app/Modules/Core/state/app.state.ts'
+    // update Store file
+    let path='/src/app/Modules/Core/state/app.state.ts';
+    let newNode=`${options.name}Red:${options.name}Reducer,`;
+    let parentNode=`appReducer`;
+    // add feature reducer to appReducer
+    addToNode(tree,path,newNode,parentNode);
+    let importNode=`import { ${options.name}Reducer } from "src/app/${options.name}/${options.name}.Data/${options.name}.State/${options.name}.reducer";`;
+    // add import reducer with path
+    addImportToFile(tree,path,importNode);
 
-    let change = addObjectToArrayChange(`${options.name}Red:${options.name}Reducer,`, tree,"appReducer",path);
+    path='/src/main.ts';
+    newNode=`provideStore`;
+    parentNode=`providers`;
+    // add feature reducer to appReducer
+    addToNode(tree,path,newNode,parentNode);    
 
-    const declarationRecorder = tree.beginUpdate(path);
-    if (change instanceof InsertChange) {
-        declarationRecorder.insertLeft(change.pos,change.toAdd);
-    }
-    let importChange=new InsertChange(path, 0,`import { ${options.name}Reducer } from "src/app/${options.name}/${options.name}.Data/${options.name}.State/${options.name}.reducer";`+'\n' )
-
-    if (importChange instanceof InsertChange) {
-      declarationRecorder.insertLeft(importChange.pos,importChange.toAdd);
-  }
     
-    tree.commitUpdate(declarationRecorder); // commits the update on the tree
+    newNode=`{ provide: I${options.name}sRepository, useClass: ${options.name}sRepository },`;
+    parentNode=`providers`;
+    // add feature reducer to appReducer
+    addToNode(tree,path,newNode,parentNode);  
 
-
-
-
-    //************************************************************************************* */    
+ 
 
     const templateSource = apply(
       url('./files'), [
