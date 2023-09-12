bb6dd51d7dc85316f8ea292bcc43b2bf8bbdde0c
GuitounYoucef <guitxyoucef@gmail.com>
2023-09-12 21:42:35 +0100

    import

--- projects/simple-feature/schematics/ui-component/index.ts
+++ projects/simple-feature/schematics/ui-component/index.ts
@@ -37,42 +37,10 @@ export function addObjectToArrayChange (context: string, tree: Tree, nodeName:st
   if (!providersNode || !providersNode.parent) {
     throw new SchematicsException(`providers people variable in ${path}`);
   }
-  showTree(node);
-  const recorder = tree.beginUpdate(path);
-  
-  applyToUpdateRecorder(recorder,
-    addImportToModule(node, path, 'mesgReducer', 'src/app/Mesg/Mesg.Data/Mesg.State/mesg.reducer')
-  );
-
-  tree.commitUpdate(recorder);
-  // define providersNode's sibling nodes and remove peopleNode from it
-/*   let providersNodeSiblings = providersNode.parent.getChildren();
-  showTree(providersNode.getChildAt(0));
-  let providersNodeIndex = providersNodeSiblings.indexOf(providersNode);
-  providersNodeSiblings = providersNodeSiblings.slice(providersNodeIndex);
-
-  
-  _context.logger.info("providersNodeSiblings ********************"+providersNodeSiblings.toString);
-
-  // get providers array literal experssion from the siblings, this means this sign "["
-  let providersArrayLiteralExpressionNode = providersNodeSiblings.find(n => n.kind === ts.SyntaxKind.ArrayLiteralExpression);
-
-  if (!providersArrayLiteralExpressionNode) {
-    throw new SchematicsException(`providers ArrayLiteralExpression node is not defined`);
-  }
-
-  // get people array list node which is in the children of peopleArrayLiteralExpressionNode and its kind of SyntaxList
- let providersListNode = providersArrayLiteralExpressionNode.getChildren().find(n => n.kind === ts.SyntaxKind.SyntaxList);
-
- if (!providersListNode) {
-     throw new SchematicsException(`providers list node is not defined`);
- } */
-
- // insert the new person to the end of the array (at the end of the peopleListNode
  return new InsertChange(path, providersNode.getEnd()+3,'\n' + context)
 }
 
-
+/*
 function showTree(node: ts.Node, indent: string = '    '): void {
   // will output the syntax kind of the node
   console.log(indent + ts.SyntaxKind[node.kind]);
@@ -86,7 +54,7 @@ function showTree(node: ts.Node, indent: string = '    '): void {
     showTree(child, indent + '    ');
   }
 } 
-
+*/
 function unppercase(str: string): string {
   return str.toUpperCase();
 }
@@ -130,6 +98,11 @@ export function ComponentGenerator(options: ComponentSchema): Rule {
     if (change instanceof InsertChange) {
         declarationRecorder.insertLeft(change.pos,change.toAdd);
     }
+    let importChange=new InsertChange(path, 0,`import { ${options.name}Reducer } from "src/app/${options.name}/${options.name}.Data/${options.name}.State/${options.name}.reducer";`+'\n' )
+
+    if (importChange instanceof InsertChange) {
+      declarationRecorder.insertLeft(importChange.pos,importChange.toAdd);
+  }
     
     tree.commitUpdate(declarationRecorder); // commits the update on the tree
 
